3dbe895dd5fd91a0bc45eb243d4e5498e40b65af
Diogo Serrão <diogo.serrao@acin.pt>
2025-04-23 12:14:34 +0100

    produtos e seus estilos

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,47 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('produtos.css') }}">
+    <title>Produtos</title>
+</head>
+
+<section id="produtos" class="py-5 bg-light">
+    <div class="container">
+        <h2 class="text-center mb-5 text-success">Os Nossos Produtos</h2>
+        <div class="row g-4">
+            <div class="col-md-4">
+                <div class="card h-100 shadow-sm">
+                    <img src="https://th.bing.com/th/id/R.91f0386c054777e445d70398fd0c394b?rik=ppanF0NQl2xjzA&riu=http%3a%2f%2fwww.salepepe.it%2ffiles%2f2014%2f02%2fpapaia.jpg&ehk=oJGdFha7Jeki6HfQsbSKAaYJEdeJ92nGPkGx4im2%2b6s%3d&risl=&pid=ImgRaw&r=0"  class="card-img-top" alt="Produto 1">  
+                        <div class="card-body">
+                        <h5 class="card-title">Papaias</h5>
+                        <p class="card-text">Cultivada de forma sustentável, ideal para temperos frescos.</p>
+                        <a href="#" class="btn btn-success">Saber Mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card h-100 shadow-sm">
+                    <img src="https://th.bing.com/th/id/OIP.bi1my5kl2XsXs7zyoNGLpQHaE8?rs=1&pid=ImgDetMain" class="card-img-top" alt="Produto 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Tomates</h5>
+                        <p class="card-text">Perfeito para interiores e fácil de cuidar.</p>
+                        <a href="#" class="btn btn-success">Saber Mais</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card h-100 shadow-sm">
+                    <img src="https://sarabackmo.com/wp-content/uploads/2017/04/skillnadenstradgard_sallat_pluggbratte-945x630.jpg" class="card-img-top" alt="Produto 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Tabuleiros Plantados</h5>
+                        <p class="card-text">Feito com materiais ecológicos, prontos para serem utilizados.</p>
+                        <a href="#" class="btn btn-success">Saber Mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
