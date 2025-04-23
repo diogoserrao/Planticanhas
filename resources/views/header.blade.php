e1fc45cdec317325e2daed141c4f77b640bdecbc
Diogo Serrão <diogo.serrao@acin.pt>
2025-04-23 11:23:43 +0100

    Initial commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
+    <title>Planti</title>
+</head>
+
+<body>
+<header id="masthead" class="site-header sticky-top bg-white shadow-sm py-2" role="banner">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="btn-logo" src="https://www.plantazon.pt/static/frontend/bulbsdirect/bulbsdirect-theme-2/pt_PT/images/logo.svg" alt="Planticanhas" style="max-height: 40px;">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item mx-2">
+                            <a class="nav-link fw-medium" href="#sobrenos">Sobre Nós</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link fw-medium" href="#produtos">Produtos</a>
+                        </li>
+                        <li class="nav-item mx-2">
+                            <a class="nav-link fw-medium" href="#contactos">Contactos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
+
+    <div class="sydney-hero-area">
+
+    </div>
+
+</body>
+
+</html>
